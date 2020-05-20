9da6970cd8163e437637de4ed046617619b02866
AaronOBrien1453 <aatobrien@gmail.com>
2020-05-19 20:17:53 -0400

    lower case, no punctuation

--- word_frequency.py
+++ word_frequency.py
@@ -1,15 +1,26 @@
+
+import string
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
+punctuation = string.punctuation
 
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    opened_file = open(file)
+    text = opened_file.read()
+    no_punctuation = ""
+    for punc in text:
+        if punc not in punctuation:
+            no_punctuation = no_punctuation + punc
+    text_lower = no_punctuation.lower()
+    print(text_lower)
 
 
+##don't worry about this stuff##
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
